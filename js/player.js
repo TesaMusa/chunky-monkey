d54aa33f3ddda23438b183fece58ca85fa4f893d
Tesa Musa <kujtesamusa1@gmail.com>
2024-01-31 15:17:30 +0100

    Add the enemy class and enemy methods to create and position them

--- js/player.js
+++ js/player.js
@@ -1,40 +1,115 @@
 class Monkey {
     constructor(speed) {
-      this.x = window.innerWidth / 2 - 25; 
-      this.y = window.innerHeight - 50; 
+      this.x = window.innerWidth / 2 - 25;
+      this.y = window.innerHeight - 50;
       this.speed = speed;
       this.element = document.createElement('img');
-      this.element.src = '../images/monkey.png'; 
+      this.element.src = '../images/monkey.png';
       this.element.style.position = 'absolute';
-      this.element.style.width = '50px'; 
-      this.element.style.height = '50px'; 
+      this.element.style.width = '50px';
+      this.element.style.height = '50px';
       document.body.appendChild(this.element);
-
+  
       this.element.style.left = `${this.x}px`;
       this.element.style.top = `${this.y}px`;
+  
+      
+      this.enemies = [];
+  
+     
+    
     }
-
+  
     moveLeft() {
       this.x -= this.speed;
       this.element.style.left = `${this.x}px`;
     }
-
+  
     moveRight() {
       this.x += this.speed;
       this.element.style.left = `${this.x}px`;
     }
-
+  
     moveUp() {
       this.y -= this.speed;
       this.element.style.top = `${this.y}px`;
     }
-
+  
     moveDown() {
       this.y += this.speed;
       this.element.style.top = `${this.y}px`;
     }
-
+  
     show() {
       this.element.style.display = 'block';
     }
+    startCreatingEnemies() {
+        setInterval(() => {
+            const newEnemy = new Enemy(5);
+            this.enemies.push(newEnemy);
+            this.positionEnemies(); // Call positionEnemies after adding a new enemy
+        }, 2000);
+    }
+
+    positionEnemies() {
+        this.enemies.forEach((enemy) => {
+            enemy.element.style.left = `${Math.random() * (window.innerWidth - 50)}px`;
+            enemy.element.style.top = `${Math.random() * -100}px`;
+        });
+    }
+
+    moveEnemies() {
+        this.enemies.forEach((enemy) => {
+            enemy.y += enemy.speed;
+            enemy.element.style.top = `${enemy.y}px`;
+
+            if (this.checkCollision(enemy)) {
+                console.log('Collision with enemy!');
+                enemy.element.style.display = 'none';
+                setTimeout(() => {
+                    enemy.y = Math.random() * -100;
+                    enemy.element.style.left = `${Math.random() * (window.innerWidth - 50)}px`;
+                    enemy.element.style.display = 'block';
+                }, 5000);
+            }
+
+            if (enemy.y > window.innerHeight) {
+                enemy.y = Math.random() * -100;
+                enemy.element.style.left = `${Math.random() * (window.innerWidth - 50)}px`;
+            }
+        });
+    }
+
+    checkCollision(enemy) {
+        // Implement collision detection logic here
+        // For example, you can check if the monkey and enemy elements overlap
+        // Return true if there is a collision, otherwise return false
+        // You can use bounding box collision detection or other methods
+    }
 }
+
+      
+
+
+
+
+  
+  class Enemy {
+    constructor(speed) {
+      this.x = 0;
+      this.y = 0;
+      this.speed = speed;
+  
+      this.element = document.createElement('img');
+      this.element.src = '../images/enemy1.png';
+      this.element.style.position = 'absolute';
+      this.element.style.width = '50px';
+      this.element.style.height = '50px';
+      document.body.appendChild(this.element);
+  
+      this.element.style.left = `${this.x}px`;
+      this.element.style.top = `${this.y}px`;
+    }
+  }
+  
+  
